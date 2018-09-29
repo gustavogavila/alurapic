0fef58d350b2a165dac29832e54d285329998d01
Gustavo Avila <gustavogavila01@gmail.com>
2018-09-29 13:10:42 -0300

    Implementação do primeiro Resolver para PhotoListComponent

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+
 import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 import { Photo } from '../photo/photo';
 
@@ -13,18 +13,10 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
   
   ngOnInit(): void {
-    
-    const userName = this.activatedRoute.snapshot.params.userName;
-    
-    this.photoService
-    .listFromUser(userName)
-    .subscribe(photos => this.photos = photos)
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 
 }
