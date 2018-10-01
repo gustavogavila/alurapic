15f644e8d52d525dc941099f1ef7ea04bb89c82e
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-01 15:00:58 -0300

    Organizando o código através de sub-módulos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '../../../node_modules/@angular/core';
-import { HttpClientModule } from '../../../node_modules/@angular/common/http';
-import { CommonModule } from '../../../node_modules/@angular/common';
 
-import { PhotoComponent } from './photo/photo.componente';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
 
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescriptionPipe,
-        LoadButtonComponent
-    ],
     imports: [
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoListModule,
+        PhotoFormModule
     ]
 })
 export class PhotosModule {
