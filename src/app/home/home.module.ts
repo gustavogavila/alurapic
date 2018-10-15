857be0aee23374c65a0e1895364286ef8093984b
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-15 18:22:09 -0300

    Iniciando a validação do form de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
-    declarations: [SignInComponent]
+    declarations: [SignInComponent],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ]
 })
 export class HomeModule {
 
