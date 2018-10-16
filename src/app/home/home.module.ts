00fc357db236922a9683d47e701e9d0011ac349f
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-16 17:40:56 -0300

    Implementação da detecção da plataforma na qual determinado trecho de código está rodando

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PlatformDetectorService } from '../core/platform-detector/platform-detector.service';
 
 @NgModule({
     declarations: [SignInComponent],
