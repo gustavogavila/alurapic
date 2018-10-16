09c464dc5670bcb6ac47aacd7ff75d67daa94897
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-16 20:39:36 -0300

    Armazenando o token no localStorage

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000'
 
@@ -10,7 +11,8 @@ const API_URL = 'http://localhost:3000'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string): Observable<any> {
     return this.http.post(API_URL + '/user/login', 
@@ -19,6 +21,7 @@ export class AuthService {
       )
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(`User ${userName} authenticated with token ${authToken}`);
       }));
   }
