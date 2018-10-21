9262d43a25d757f71964d144dca3a9ec447d6502
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-21 16:05:06 -0300

    Implementação das validações do formulário de registro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
-import { PlatformDetectorService } from '../core/platform-detector/platform-detector.service';
 import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
