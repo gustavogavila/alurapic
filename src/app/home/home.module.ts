d40297612c75674b290f3c455f432d221ea6c1be
Gustavo Avila <gustavogavila01@gmail.com>
2018-10-21 15:24:18 -0300

    Implementação do componente de registro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
 import { PlatformDetectorService } from '../core/platform-detector/platform-detector.service';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [SignInComponent],
+    declarations: [
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
